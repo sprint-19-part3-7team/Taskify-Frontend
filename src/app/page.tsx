858955c1c54870d5d1cb0bd365875466b68f847b
jung518 <wjddndus0518@naver.com>
2025-11-18 16:20:04 +0900

    🐛 Fix: 컬러 변수 등록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-// components/MyButton.tsx
-import React from 'react';
-
-export default function MyButton() {
-  return <button className='bg-blue-500 px-4 py-2 text-white hover:bg-blue-600'>Click Me</button>;
+export default function Home() {
+  return <div>메인 페이지</div>;
 }

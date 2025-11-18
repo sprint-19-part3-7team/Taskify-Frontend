6d1bf29ecd5cf12600c45c2954bd3667bc89e5df
jung518 <wjddndus0518@naver.com>
2025-11-18 14:43:57 +0900

    🐛 Fix: Tailwind v4 설정 및 컬러 변수 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-  return <div>메인 페이지~</div>;
+// components/MyButton.tsx
+import React from 'react';
+
+export default function MyButton() {
+  return <button className='bg-blue-500 px-4 py-2 text-white hover:bg-blue-600'>Click Me</button>;
 }

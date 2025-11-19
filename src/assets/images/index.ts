e29464a3d8813a401fbce72fd78cc517fe804aa4
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-19 11:28:59 +0900

    [⚙️Chore/14] 이미지 파일 추가 + 이미지 import 관리 index.tsx 파일 추가  (#15)

---
+++ src/assets/images/index.ts
@@ -0,0 +1,17 @@
+import noInvitedDashboard from '@/assets/images/dashboard/no-invited-dashboard.png';
+import mainVisual from '@/assets/images/home/main-visual.png';
+import serviceCard1 from '@/assets/images/home/service-card-1.png';
+import serviceCard2 from '@/assets/images/home/service-card-2.png';
+import serviceCard3 from '@/assets/images/home/service-card-3.png';
+import serviceOverview1 from '@/assets/images/home/service-overview-1.png';
+import serviceOverview2 from '@/assets/images/home/service-overview-2.png';
+
+export default {
+  noInvitedDashboard,
+  mainVisual,
+  serviceCard1,
+  serviceCard2,
+  serviceCard3,
+  serviceOverview1,
+  serviceOverview2,
+};

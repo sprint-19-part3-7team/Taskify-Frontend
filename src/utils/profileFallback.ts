347e69b2869d2fc41c70aa390d471dd30f220d3f
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-20 14:41:16 +0900

    📝 Docs: 함수 설명 주석 추가

--- src/utils/profileFallback.ts
+++ src/utils/profileFallback.ts
@@ -1,5 +1,9 @@
 import { COLORS } from '@/constants/color';
 
+/**
+ * getMonogram
+ * 유저 닉네임 첫번째 글자를 리턴하는 함수입니다.
+ * */
 export const getMonogram = (nickname: string) => {
   if (!nickname) {
     return '';
@@ -7,6 +11,11 @@ export const getMonogram = (nickname: string) => {
   return nickname[0];
 };
 
+/**
+ * getProfileColorForId
+ * 유저 id값을 기준으로 프로필 배경 컬러값을 결정하는 함수입니다.
+ * orange, blue, green, pink, purple 문자열이 리턴됩니다.
+ * */
 export const getProfileColorForId = (userId: number) => {
   const idx = userId % COLORS.length;
   return COLORS[idx];

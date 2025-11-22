c89e770c78c21ffe156a6b8b72917f201273c5de
apple <apple@apples-MacBook-Air.local>
2025-11-22 12:00:43 +0900

    ✨ Feat: ComponentPropsWithoutRef 추가

--- src/components/common/Label.tsx
+++ src/components/common/Label.tsx
@@ -1,1 +1,14 @@
-// label 공통 컴포넌트 파일입니다.
+import type { ComponentPropsWithoutRef } from 'react';
+
+interface LabelProps extends ComponentPropsWithoutRef<'label'> {
+  htmlFor?: string; // 연결할 input id
+  children: React.ReactNode;
+}
+
+export default function Label({ htmlFor, children, className, ...props }: LabelProps) {
+  return (
+    <label htmlFor={htmlFor} className={className} {...props}>
+      {children}
+    </label>
+  );
+}

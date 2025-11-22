88364fcd94cfa3a538aadfdca71430f30e4a5cf1
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-22 19:20:56 +0900

    ♻️ Refactor: role 속성 추가

--- src/components/dashboard/dropdown/DropdownMenuCheckList.tsx
+++ src/components/dashboard/dropdown/DropdownMenuCheckList.tsx
@@ -6,7 +6,9 @@ export default function DropdownMenuCheckList({ children }: { children: React.Re
 
   return (
     isOpen && (
-      <ul className='absolute top-[calc(100%+8px)] left-0 z-1000 w-full rounded-md border border-gray-300 bg-gray-0'>
+      <ul
+        role='menu'
+        className='absolute top-[calc(100%+8px)] left-0 z-1000 w-full rounded-md border border-gray-300 bg-gray-0'>
         {children}
       </ul>
     )
